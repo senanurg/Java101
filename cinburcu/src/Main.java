efa5530b6080ed935d7c5a84dc9310301639eb28
Sena Nur <151245979+senanurg@users.noreply.github.com>
2024-01-07 12:17:48 +0300

    çin

---
+++ cinburcu/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz: ");
+        int dogumYili = scanner.nextInt();
+
+        // Çin Zodyağı hesaplama
+        int zodyakIndex = dogumYili % 12;
+        String zodyak = "";
+
+        switch (zodyakIndex) {
+            case 0:
+                zodyak = "Maymun";
+                break;
+            case 1:
+                zodyak = "Horoz";
+                break;
+            case 2:
+                zodyak = "Köpek";
+                break;
+            case 3:
+                zodyak = "Domuz";
+                break;
+            case 4:
+                zodyak = "Fare";
+                break;
+            case 5:
+                zodyak = "Öküz";
+                break;
+            case 6:
+                zodyak = "Tiger";
+                break;
+            case 7:
+                zodyak = "Tavşan";
+                break;
+            case 8:
+                zodyak = "Ejderha";
+                break;
+            case 9:
+                zodyak = "Yılan";
+                break;
+            case 10:
+                zodyak = "At";
+                break;
+            case 11:
+                zodyak = "Koyun";
+                break;
+            default:
+                System.out.println("Hatalı veri girişi!");
+                return;
+        }
+
+        System.out.println("Çin Zodyağı Burcunuz: " + zodyak);
+
+        scanner.close();
+    }
+}
